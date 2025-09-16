9d060b2ed064515fb61f8c3f58974af50d9709e2
Edward Nguyen <68344387+dean6969@users.noreply.github.com>
2025-09-16 19:28:55 +1000

    Update remove_pipeline.sh

--- remove_pipeline.sh
+++ remove_pipeline.sh
@@ -3,14 +3,14 @@ set -e
 
 echo "🛑 Stopping and removing containers, networks, volumes, and images for this project..."
 
-# Dừng và xóa toàn bộ resource của docker-compose.yml
+# Stop and remove all docker conatainer and volumes của docker-compose.yml
 docker compose down -v --remove-orphans
 
-# Xóa tất cả images có prefix theo tên project (event-duck-pipeline)
+# Delete all images (event-duck-pipeline)
 PROJECT_NAME=$(basename "$PWD")
 docker images --format "{{.Repository}}:{{.Tag}}" | grep "$PROJECT_NAME" | xargs -r docker rmi -f
 
-# Xóa luôn dangling & unused images
+# remove dangling & unused images
 docker image prune -af
 
 echo "✅ Project resources fully removed (containers, networks, volumes, and related images)."
